028c6c8fbd11917076c00b7ef445a29c0e963528
kimyeee <kimyeee@163.com>
2019-02-25 00:20:52 +0800

    change

--- movieweb/videoplay/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from videoplay.models import Movie, User, UserComment, MoviePay
-# Register your models here.
-
-admin.site.register(Movie)
-admin.site.register(User)
-admin.site.register(UserComment)
-admin.site.register(MoviePay)
